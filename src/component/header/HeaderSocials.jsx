27de3a6778a64d7319caa65641ee6e46a8a939f8
Shaon <Bshaon19@gmail.com>
2022-08-26 23:40:12 +0530

    bug fix 2

--- src/component/header/HeaderSocials.jsx
+++ src/component/header/HeaderSocials.jsx
@@ -6,8 +6,8 @@ import {BsGithub} from 'react-icons/bs'
 const HeaderSocials = () => {
   return (
     <div className='header_socials'>
-        <a href="https://linkedin.com" target="_blank"> <BsLinkedin/></a>
-        <a href="https://github.com/shaon046" target="_blank"><BsGithub/></a>
+        <a href="https://linkedin.com" target="_blank" rel="noreferrer"> <BsLinkedin/></a>
+        <a href="https://github.com/shaon046" target="_blank" rel="noreferrer"><BsGithub/></a>
     </div>
   )
 }
